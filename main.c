8690ca7d231a17d2a79d7f4b641d75ad18a1c609
Minyoung427 <55172353+Minyoung427@users.noreply.github.com>
2019-11-26 15:31:18 +0900

    1-2

--- main.c
+++ main.c
@@ -8,8 +8,15 @@
 		double grade; 
 	};
 
-
 int main(int argc, char *argv[]) {
+
 	struct student s1 = {24, "Min Young", 4.3}; 
+	s1.ID = 123456;
+	strcpy(s1.name,"MinYoung");
+	s1.grade = 4.2;
+	
+	printf("ID : %d\n",s1.ID);
+	printf("name:%s\n",s1.name);
+	printf("grade:%f\n",s1.grade);
 	return 0;
 }
